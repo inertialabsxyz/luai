da3640f9eabe1772e5a23096ae957c19476b79ee
Andy Bell <andyjsbell@gmail.com>
2026-03-04 19:51:30 +0100

    fix: script messaging

--- zkvm-prove.sh
+++ zkvm-prove.sh
@@ -3,11 +3,7 @@ cargo run -- ../examples/prover.lua /tmp/compiled.json
 cd ../prover
 cargo run -- /tmp/compiled.json /tmp/dry_result.json
 cd ../openvm
-# echo "keygen..."
-# cargo openvm keygen
-# echo "commit..."
-# cargo openvm commit
-echo "encode inputs"
+echo "encode inputs for openvm"
 cargo run --bin luai-openvm-encoder -- /tmp/compiled.json /tmp/dry_result.json
 echo "run circuit"
 cargo openvm run --bin luai-openvm --input /tmp/openvm-1.json
